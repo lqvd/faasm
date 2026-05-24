12282b1d715f21ab5430cb64626ca701ad04addb
Liam Doan <lqd22@ic.ac.uk>
2026-05-24 01:04:35 +0100

    feat: update dist tests to test long-running service

--- tests/dist/rpc/test_migration.cpp
+++ tests/dist/rpc/test_migration.cpp
@@ -2,6 +2,8 @@
 #include "fixtures.h"
 #include <faabric/scheduler/Scheduler.h>
 #include <faabric/util/batch.h>
+#include <faabric/transport/common.h>
+#include <faabric/rpc/RpcTransportClient.h>
 
 namespace tests {
 
@@ -21,121 +23,60 @@ class RpcDistTestsFixture : public DistTestsFixture
 };
 
 TEST_CASE_METHOD(DistTestsFixture,
-                 "Test migrating a coroutine RPC function",
-                 "[rpc][migration]")
+                 "Test basic RPC service invocation",
+                 "[rpc][service]")
 {
-    updatePlannerPolicy("spot");
     setLocalRemoteSlots(4, 4);
 
-    auto req = faabric::util::batchExecFactory("rpc", "migrate_rpc", 1);
-    faabric::Message& msg = req->mutable_messages()->at(0);
+    // Start ping service as long-running SERVICE on host A
+    auto svcReq = faabric::util::batchExecFactory("rpc", "PingSvc", 1);
+    svcReq->set_type(faabric::BatchExecuteRequest::SERVICE);
+    faabric::Message& msg = svcReq->mutable_messages()->at(0);
     msg.set_isrpc(true);
+    
+    auto svcDec = std::make_shared<batch_scheduler::SchedulingDecision>(
+        svcReq->appid(), svcReq->groupid());
+    svcDec->addMessage(getDistTestMasterIp(), 0, 0, 0);
+    plannerCli.preloadSchedulingDecision(svcDec);
+    plannerCli.callFunctions(svcReq);
 
-    std::string hostBefore = getDistTestMasterIp();
-    std::string hostAfter = getDistTestWorkerIp();
+    // Give the service time to register itself (planner registers via callBatch)
+    SLEEP_MS(500);
 
-    auto preloadDec = std::make_shared<batch_scheduler::SchedulingDecision>(
-        req->appid(), req->groupid());
-    preloadDec->addMessage(hostBefore, 0, 0, 0);
-    plannerCli.preloadSchedulingDecision(preloadDec);
+    // Verify discovery finds it
+    auto endpoint = plannerCli.resolveServiceEndpoint("rpc/PingSvc");
+    REQUIRE(endpoint.has_value());
+    REQUIRE(endpoint->host() == getDistTestMasterIp());
 
-    setNextEvictedVmIp({ hostBefore });
+    // Run the client
+    auto clientReq = faabric::util::batchExecFactory("rpc", "migrate_rpc", 1);
+    faabric::Message& clientMsg = clientReq->mutable_messages()->at(0);
+    clientMsg.set_isrpc(true);
+    plannerCli.callFunctions(clientReq);
 
-    plannerCli.callFunctions(req);
+    auto clientResults = waitForBatchResults(clientReq, 1);
+    REQUIRE(clientResults->messageresults_size() == 1);
+    REQUIRE(clientResults->messageresults(0).returnvalue() == 0);
 
-    // Wait for the single message result
-    auto batchResults = waitForBatchResults(req, 1);
-    REQUIRE(batchResults->messageresults_size() == 1);
+    // Verify all four pings made it into the output
+    const auto& output = clientResults->messageresults(0).outputdata();
+    REQUIRE(output.find("Pong: Hello from host A") != std::string::npos);
+    REQUIRE(output.find("Pong: Hello from wherever we are now") != std::string::npos);
+    REQUIRE(output.find("Pong: Fan-out C") != std::string::npos);
+    REQUIRE(output.find("Pong: Fan-out D") != std::string::npos);
 
-    const auto& result = batchResults->messageresults(0);
-    REQUIRE(result.returnvalue() == 0);
-    REQUIRE(result.executedhost() == hostAfter);
+     // Tell the service to shut down
+    faabric::rpc::RpcTransportClient ctrl(
+        endpoint->host(), RPC_ASYNC_PORT, RPC_SYNC_PORT, 5000);
 
-    updatePlannerPolicy("bin-pack");
-}
+    faabric::RpcShutdownRequest shutdownReq;
+    shutdownReq.set_targetappid(endpoint->appid());
+    shutdownReq.set_targetmessageid(endpoint->messageid());
+    ctrl.asyncSendShutdown(shutdownReq);
 
-TEST_CASE_METHOD(RpcDistTestsFixture,
-                 "RPC forwarding of in-flight response",
-                 "[rpc][migration][forwarding]")
-{
-    updatePlannerPolicy("spot");
-    setLocalRemoteSlots(4, 4);
-
-    auto req = faabric::util::batchExecFactory("rpc", "rpc_forwarding", 1);
-    faabric::Message& msg = req->mutable_messages()->at(0);
-    msg.set_isrpc(true);
-
-    std::string hostBefore = getDistTestMasterIp();
-    std::string hostAfter = getDistTestWorkerIp();
-
-    setupRpcMigrationTest(req, hostBefore);
-    plannerCli.callFunctions(req);
-
-    auto batchResults = waitForBatchResults(req, 1);
-    REQUIRE(batchResults->messageresults_size() == 1);
-
-    auto result = batchResults->messageresults(0);
-    REQUIRE(result.returnvalue() == 0);
-    REQUIRE(result.executedhost() == hostAfter);
-    REQUIRE(result.executedhost() != hostBefore);
-
-    updatePlannerPolicy("bin-pack");
-}
-
-TEST_CASE_METHOD(RpcDistTestsFixture,
-                 "RPC pending response survives migration",
-                 "[rpc][migration][pending]")
-{
-    updatePlannerPolicy("spot");
-    setLocalRemoteSlots(4, 4);
-
-    auto req = faabric::util::batchExecFactory("rpc", "rpc_pending", 1);
-    faabric::Message& msg = req->mutable_messages()->at(0);
-    msg.set_isrpc(true);
-
-    std::string hostBefore = getDistTestMasterIp();
-    std::string hostAfter = getDistTestWorkerIp();
-
-    setupRpcMigrationTest(req, hostBefore);
-    plannerCli.callFunctions(req);
-
-    auto batchResults = waitForBatchResults(req, 1);
-    REQUIRE(batchResults->messageresults_size() == 1);
-
-    auto result = batchResults->messageresults(0);
-    REQUIRE(result.returnvalue() == 0);
-    REQUIRE(result.executedhost() == hostAfter);
-    REQUIRE(result.executedhost() != hostBefore);
-
-    updatePlannerPolicy("bin-pack");
-}
-
-TEST_CASE_METHOD(RpcDistTestsFixture,
-                 "RPC cached response survives migration",
-                 "[rpc][migration][cached]")
-{
-    updatePlannerPolicy("spot");
-    setLocalRemoteSlots(4, 4);
-
-    auto req = faabric::util::batchExecFactory("rpc", "rpc_cached", 1);
-    faabric::Message& msg = req->mutable_messages()->at(0);
-    msg.set_isrpc(true);
-
-    std::string hostBefore = getDistTestMasterIp();
-    std::string hostAfter = getDistTestWorkerIp();
-
-    setupRpcMigrationTest(req, hostBefore);
-    plannerCli.callFunctions(req);
-
-    auto batchResults = waitForBatchResults(req, 1);
-    REQUIRE(batchResults->messageresults_size() == 1);
-
-    auto result = batchResults->messageresults(0);
-    REQUIRE(result.returnvalue() == 0);
-    REQUIRE(result.executedhost() == hostAfter);
-    REQUIRE(result.executedhost() != hostBefore);
-
-    updatePlannerPolicy("bin-pack");
+    auto svcResults = waitForBatchResults(svcReq, 1);
+    REQUIRE(svcResults->messageresults_size() == 1);
+    REQUIRE(svcResults->messageresults(0).returnvalue() == 0);
 }
 
 } // namespace tests
